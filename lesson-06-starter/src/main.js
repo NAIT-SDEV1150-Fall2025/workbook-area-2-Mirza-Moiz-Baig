c3993a507dd13f5dee217b861836282aa407ec29
MIRZA MOIZ BAIG <baigmirzamoiz1@gmail.com>
2026-05-29 18:48:54 -0600

    added lesson06 completed and lesson 08 starter

--- lesson-06-starter/src/main.js
+++ lesson-06-starter/src/main.js
@@ -2,7 +2,7 @@ console.log('Lesson 06 starter loaded');
 
 // Selecting elements
 const titleEl = document.querySelector('#page-title');
-const taglineEl = document.querySelector('.tagline');
+const taglineEl = document.querySelector('.tagline');// query selector selects the first occurence only
 const heroImg = document.querySelector('#hero-img');
 const heroCaption = document.querySelector('#hero-caption');
 const dynamicBox = document.querySelector('#dynamic-box');
@@ -10,9 +10,12 @@ const footerNote = document.querySelector('#footer-note');
 
 // 1. Create a new variable for the feature list element
 const featureList = document.getElementById('feature-list');
+
+// 3. Modify list content
 const li = document.createElement('li');// <li></li>
 li.className = 'feature';// <li class = 'feature'></li>
 li.textContent = 'Flexible';// <li class = 'feature'>Flexible</li>
+// 4. Add a new item dynamically
 featureList.appendChild(li);// This adds the child "li" to the html document
 
 // 2. Add feature list to the displayed elements below
@@ -20,20 +23,34 @@ console.log('Selected elements:', {
   titleEl, taglineEl, heroImg, heroCaption, dynamicBox, footerNote,
 });
 
-// 3. Modify list content
-
-// 4. Add a new item dynamically
-
 // 5. Retreive all list items (querySelectorAll) and update their text
+const features = document.querySelectorAll('.feature');
+console.log(features);
+features.forEach((li, idx) => {
+  li.textContent = `${idx + 1}. ${li.textContent}`;
+}
+);
+// ()=>{} arrow functions are not named and usually limited to a particular block of code
 
 // 6. Removing the first item from the list using DOM relationships to find it
+// .removeChild: removes the element from the children of the parent node
+
+featureList.removeChild(featureList.firstElementChild);
 
 // 7. Update the second item using nextElementSibling
-
+featureList.firstElementChild.nextElementSibling.textContent += `(updated)`;
 // 8. Move the last item to the front of the list
+const last = featureList.removeChild(featureList.lastChild);
+// insertBefore(<the element to be inserted, the location where you want to insert)
+featureList.insertBefore(last, featureList.firstChild);
 
 // 9. Use a timer to add a new item after 3 seconds have passed
-
+setTimeout(() => {
+  const newElement = document.createElement('li');
+  newElement.className = 'feature';
+  newElement.textContent = `added after 3 secs`;
+  featureList.appendChild(newElement);
+}, 3000);// 3000 miliseconds = 3 secs.
 // **** THE FOLLOWING IS EXISTING CODE FROM LESSON 05
 
 // textContent vs innerHTML
