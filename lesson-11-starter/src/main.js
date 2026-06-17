4a2f6b26e746e3f039640af69ee1c08978892202
MIRZA MOIZ BAIG <baigmirzamoiz1@gmail.com>
2026-06-16 18:39:34 -0600

    lesson-11 complete

--- lesson-11-starter/src/main.js
+++ lesson-11-starter/src/main.js
@@ -26,7 +26,7 @@ function serializeForm(formEl) {
 }
 
 form.addEventListener('submit', (e) => {
-  e.preventDefault();
+  e.preventDefault();// webpage never reloads and doesnt submit data to the server
 
   const data = serializeForm(form);
 
@@ -38,8 +38,8 @@ form.addEventListener('submit', (e) => {
   // OPTIONAL - use the following alongside the `novalidate` form attribute
   // to trigger built-in HTML validation
   // if (form.checkValidity()) {
-  
-  result.textContent = `
+  if (form.checkValidity()) {
+    result.textContent = `
     Submission received:
     - Name: ${data.fullName}
     - Email: ${data.email}
@@ -47,7 +47,7 @@ form.addEventListener('submit', (e) => {
     - Plan: ${data.plan}
     - Topics: ${data.topics}
   `;
-  // } // end if form.checkValidity()
+  }
 });
 
 form.addEventListener('reset', () => {
@@ -55,12 +55,34 @@ form.addEventListener('reset', () => {
 });
 
 // 1. Add validation logic to the form on 'input' events
-
-// 1.1 custom validation for fullName (must contain two words)
-
-// 1.2 custom validation for bio (minimum length)
-
-// 1.3 custom validation for email (basic '@' symbol check)
-
-// 1.4 report the validity status to the user
-  
+form.addEventListener('input', (event) => {
+  const target = event.target;
+  // 1.1 custom validation for fullName (must contain two words)
+  if ((target.name) === 'fullName') {
+    const nameParts = target.value.trim().split(' '); // holds an array
+    if (nameParts.length < 2) {
+      target.setCustomValidity('Full name must contain atleast 2 words.');
+    } else {
+      target.setCustomValidity(''); // clearing the error message
+    }
+  }
+  // 1.2 custom validation for bio (minimum length = 40 words)
+  if ((target.name) === 'bio') {
+    const bio = target.value.trim().split(' '); // holds an array
+    if (bio.length < 40) {
+      target.setCustomValidity('Full name must contain atleast 40 words.');
+    } else {
+      target.setCustomValidity(''); // clearing the error message
+    }
+  }
+  // 1.3 custom validation for email (basic '@' symbol check)
+  if (target.name === 'email') {
+    if (!target.value.includes('@')) {
+      target.setCustomValidity('Email must contain @ symbol.');
+    } else {
+      target.setCustomValidity('');
+    }
+  }
+  // 1.4 report the validity status to the user
+  target.reportValidity();
+});
