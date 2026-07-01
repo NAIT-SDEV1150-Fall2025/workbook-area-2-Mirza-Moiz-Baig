943b7188cf8e91489ec0caa8580c54718204540b
MIRZA MOIZ BAIG <baigmirzamoiz1@gmail.com>
2026-06-30 20:17:43 -0600

    added lesson-16- partial code

--- lesson-16-starter/src/main.js
+++ lesson-16-starter/src/main.js
@@ -1,11 +1,20 @@
 // Import the functions necessary to make the API calls
-
+import { fetchData, postData } from "./utils";
 // Select the necessary DOM elements
-
+const loadButton = document.getElementById('loadBooks');
+const addForm = document.getElementById('addBook');
+const list = document.getElementById('bookList');
 // Define the API endpoint
-
+const endpoint = 'http://localhost:3000/books';
 // Define a function to handle loading and displaying the list of books
+async function loadHandler() {
+  try {
+    const books = await fetchData(endpoint);
+  }
+  catch (error) {
 
+  }
+}
 // Define a function to handle form submission for adding a new book
 
 // Attach event listeners to the button and form
