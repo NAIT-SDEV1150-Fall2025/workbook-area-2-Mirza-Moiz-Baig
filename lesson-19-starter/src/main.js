8eb09dac60228200d1ec543c4aecf27cd253d8b5
MIRZA MOIZ BAIG <baigmirzamoiz1@gmail.com>
2026-07-17 17:59:46 -0600

    added lesson 19-complete

--- lesson-19-starter/src/main.js
+++ lesson-19-starter/src/main.js
@@ -1,5 +1,23 @@
 // Import the user-card component to register the custom element
 import './user-card.js';
 // Create an additional user card using HTML and append it to the main element
+const dynamicUserCard = `
+     <user-card avatar="https://placehold.co/80x80/7700ff/ffffff">
+  <span slot="name">Mipha</span>
+  <span slot="description">Zora Champion</span>
+</user-card>`;
 
+const mainTag = document.querySelector('main');
+mainTag.insertAdjacentHTML('beforeend', dynamicUserCard);
 // Create another user card using JavaScript DOM API only and append it to the main element
+const anotherUserCard = document.createElement('user-card');
+anotherUserCard.setAttribute('avatar', 'https://placehold.co/80x80/770000/ffffff');
+const nameSpan = document.createElement('span');
+nameSpan.setAttribute('slot', 'name');
+nameSpan.textContent = 'Yunobo';
+const descSpan = document.createElement('span');
+descSpan.setAttribute('slot', 'description');
+descSpan.textContent = 'President of YunoboCo';
+anotherUserCard.appendChild(nameSpan);
+anotherUserCard.appendChild(descSpan);
+mainTag.appendChild(anotherUserCard);
